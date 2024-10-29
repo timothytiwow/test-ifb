285dfeab0231b775c66aa659ecffd265cfc11d56
Timothy Merfry Tiwow <merfrytiwow@bigdade.id>
2024-10-29 17:00:16 +0800

    soal nomor 10

--- soal_10_dan_11/server.js
+++ soal_10_dan_11/server.js
@@ -2,8 +2,28 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("Server's running!")
+})
+
+app.get('/api/v1/getData', (req, res) => {
+    console.log(`Received GET request on /api/v1/getData`)
+
+    res.json({
+        status: "200",
+        data: "Hallo!"
+    })
+})
+
+app.post('/api/v1/postData', (req, res) => {
+    console.log(`Received POST request on /api/v1/postData with payload: ${JSON.stringify(req.body)}`)
+
+    res.json({
+        status: "200",
+        data: `Data received successfully! echo: ${JSON.stringify(req.body)}`
+    })
 })
 
 app.listen(port, () => {
