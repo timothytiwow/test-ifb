e3b6735943b34a0629f75c83ca576ab10c8d3168
Timothy Merfry Tiwow <merfrytiwow@bigdade.id>
2024-10-29 14:54:56 +0800

    soal nomor 3

--- soal_1_sampai_7/app/src/App.js
+++ soal_1_sampai_7/app/src/App.js
@@ -6,16 +6,31 @@ const data = require('./data/myJsonArrayObject.json')
 
 function App() {
   const [jsonData, setJsonData] = React.useState(data)
+  
+  //supporting soal nomor 4
+  const [fetchedData, setFetchedData] = React.useState(null)
 
   //soal nomor 2
   const changeValue = () => {
     setJsonData(jsonData.map(el => el.id === 0 ? {...el, nama: "Splinter"} : el))
   }
 
+  //soal nomor 3
+  const fetchData = () => {
+    fetch('http://jsonplaceholder.typicode.com/posts')
+      .then(resp => resp.json())
+      .then(json => {
+        console.log(json)
+
+        setFetchedData(json)
+      })
+  }
+
   return (
     <div>
       <div style={{
-        display: 'flex'
+        display: 'flex',
+        flexDirection: 'column'
       }}>
         <div style={{
           marginLeft: 'auto',
@@ -27,6 +42,19 @@ function App() {
           <p><b>Soal 2</b></p>
           <p style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(jsonData, null, 2)}</p>
           <button onClick={changeValue}>Change Value</button>
+        </div>
+
+        <div style={{
+          marginTop: '1em',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          backgroundColor: 'white',
+          borderRadius: 10,
+          padding: '1em'
+        }}>
+          <p><b>Soal 3</b></p>
+          <p> Tekan Fetch untuk mengambil data, data akan ditampilkan di konsol (F12)</p>
+          <button onClick={fetchData}>Fetch</button>
         </div>
       </div>
     </div>
