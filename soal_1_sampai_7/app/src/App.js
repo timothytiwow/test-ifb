f341c937352602d3fb00b6fa7cecab569bd68b5c
Timothy Merfry Tiwow <merfrytiwow@bigdade.id>
2024-10-29 15:12:46 +0800

    soal nomor 4

--- soal_1_sampai_7/app/src/App.js
+++ soal_1_sampai_7/app/src/App.js
@@ -6,13 +6,13 @@ const data = require('./data/myJsonArrayObject.json')
 
 function App() {
   const [jsonData, setJsonData] = React.useState(data)
-  
+
   //supporting soal nomor 4
   const [fetchedData, setFetchedData] = React.useState(null)
 
   //soal nomor 2
   const changeValue = () => {
-    setJsonData(jsonData.map(el => el.id === 0 ? {...el, nama: "Splinter"} : el))
+    setJsonData(jsonData.map(el => el.id === 0 ? { ...el, nama: "Splinter" } : el))
   }
 
   //soal nomor 3
@@ -22,7 +22,7 @@ function App() {
       .then(json => {
         console.log(json)
 
-        setFetchedData(json)
+        setFetchedData(json.slice(0, 10))
       })
   }
 
@@ -30,24 +30,23 @@ function App() {
     <div>
       <div style={{
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingLeft: '2em',
+        paddingRight: '2em',
       }}>
         <div style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
           backgroundColor: 'white',
           borderRadius: 10,
           padding: '1em'
         }}>
           <p><b>Soal 2</b></p>
-          <p style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(jsonData, null, 2)}</p>
+          <p style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(jsonData, null, 2)}</p>
           <button onClick={changeValue}>Change Value</button>
         </div>
 
         <div style={{
           marginTop: '1em',
-          marginLeft: 'auto',
-          marginRight: 'auto',
           backgroundColor: 'white',
           borderRadius: 10,
           padding: '1em'
@@ -55,6 +54,35 @@ function App() {
           <p><b>Soal 3</b></p>
           <p> Tekan Fetch untuk mengambil data, data akan ditampilkan di konsol (F12)</p>
           <button onClick={fetchData}>Fetch</button>
+        </div>
+
+        <div style={{
+          marginTop: '1em',
+          backgroundColor: 'white',
+          borderRadius: 10,
+          padding: '1em',
+        }}>
+          <p><b>Soal 4</b></p>
+          <p>Tabel akan ditampilkan ketika tombol Fetch di soal nomor 3 ditekan</p>
+          {
+            fetchedData === null ?
+              <p>Tekan fetch di Soal 3 diatas</p> :
+              <div style={{ height: '25em', overflowY: 'scroll' }}>
+                <table>
+                  <tr>
+                    {Object.keys(fetchedData[0]).map((el, idx) => <th id={idx}>{el}</th>)}
+                  </tr>
+                  {fetchedData.map(el =>
+                    <tr id={el.id}>
+                      <td>{el.userId}</td>
+                      <td>{el.id}</td>
+                      <td>{el.title}</td>
+                      <td>{el.body}</td>
+                    </tr>
+                  )}
+                </table>
+              </div>
+          }
         </div>
       </div>
     </div>
