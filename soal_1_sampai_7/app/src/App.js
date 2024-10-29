1c7b12bf676dc6b473ef5b4e35b483a9925154cc
Timothy Merfry Tiwow <merfrytiwow@bigdade.id>
2024-10-29 14:49:42 +0800

    soal nomor 2

--- soal_1_sampai_7/app/src/App.js
+++ soal_1_sampai_7/app/src/App.js
@@ -1,12 +1,34 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 //soal nomor 1
-import './data/myJsonArrayObject.json'
+const data = require('./data/myJsonArrayObject.json')
 
 function App() {
+  const [jsonData, setJsonData] = React.useState(data)
+
+  //soal nomor 2
+  const changeValue = () => {
+    setJsonData(jsonData.map(el => el.id === 0 ? {...el, nama: "Splinter"} : el))
+  }
+
   return (
     <div>
-
+      <div style={{
+        display: 'flex'
+      }}>
+        <div style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          backgroundColor: 'white',
+          borderRadius: 10,
+          padding: '1em'
+        }}>
+          <p><b>Soal 2</b></p>
+          <p style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(jsonData, null, 2)}</p>
+          <button onClick={changeValue}>Change Value</button>
+        </div>
+      </div>
     </div>
   );
 }
